1b17994df7d6568563a5fee1af01b457a964815e
MUKIL1175 <144776208+MUKIL1175@users.noreply.github.com>
2025-01-09 21:34:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import os
 import torch
 from TTS.api import TTS
 import gradio as gr
 
+if not os.path.exists("outputs"):
+    os.makedirs("outputs")
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def gen_audio(text = "hello nisha i am mukkil "):
-    tts = TTS(model_name = 'tts_models/en/ljspeech/fast_pitch').to(device)
-    tts.tts_to_file(text = text,file_path = "outputs/output.wav")
-    return "output/output.wav"
+def gen_audio(text="hello nisha i am mukkil "):
+    tts = TTS(model_name='tts_models/en/ljspeech/fast_pitch').to(device)
+    tts.tts_to_file(text=text, file_path="outputs/output.wav")
+    return "outputs/output.wav"  # Make sure the correct path is returned
 
-#use this to check the audio out
+# Use this to check the audio out
 #print(gen_audio())
 
 demo = gr.Interface(
-    fn = gen_audio,
-    inputs=[gr.Text(label="Text"),],
-    outputs=[gr.Audio(label="Audio"),],
+    fn=gen_audio,
+    inputs=[gr.Text(label="Text"), ],
+    outputs=[gr.Audio(label="Audio"), ],
 )
 demo.launch()
